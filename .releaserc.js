51932dd8d1bc6965a1f300a3b1c24098a5b5cad1
Anton Chertov <anton@athyria.com>
2021-02-28 18:22:44 +0300

    chore: semantic release and circleCi

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: ['main'],
+  ci: true,
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json'],
+        message:
+            'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
